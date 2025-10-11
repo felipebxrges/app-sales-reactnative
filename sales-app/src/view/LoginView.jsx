9ebacc2331628cea238bd31134fa0156eb591a37
Felipe Borges <borgesfelipe2005@gmail.com>
2025-10-11 17:29:27 -0300

    adding login view

---
+++ sales-app/src/view/LoginView.jsx
@@ -0,0 +1,92 @@
+import { Button, Card, Snackbar, Text, TextInput } from "react-native-paper"
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, StyleSheet, KeyboardAvoidingView } from "react-native";
+import { useEffect, useState } from "react";
+import { useAuth } from "./components/authProvider/AuthProvider";
+
+
+const LoginView = ( {navigation} )=>{
+
+    const [showPass, setShowPass] = useState(true)
+    const [campos, setCampos] = useState({email: '',senha: ''})
+    const [showSnack, setShowSnack] = 
+        useState({ show: false, message:'Login ou Senha incorreta.'})
+        
+    const {login} = useAuth()
+
+    useEffect(()=>{
+        const id = setTimeout(() => {
+          setShowSnack( {...showSnack, show: false } )
+        }, 3000);
+        return ()=> clearTimeout(id)
+    },[showSnack])
+
+    const handleLogin = ()=>{
+      console.log(campos)
+      if (login(campos.email, campos.senha)){
+          navigation.replace('AppTabs')
+      }else{
+        setCampos( {email: '',senha: ''} )
+        setShowSnack( {...showSnack, show: true} )
+      }
+    }
+
+    return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <KeyboardAvoidingView style={{flex: 1}}>
+        <Pressable onPress={()=>{Keyboard.dismiss()}}>
+
+        <Card mode="elevated">
+          <Card.Title   />
+          <Card.Content>
+            <Text variant="titleLarge">LOGIN</Text>
+
+            <TextInput mode="outlined" 
+            left={<TextInput.Icon icon="email" />}
+            value={campos.email}
+            onChangeText={(e)=> setCampos( {...campos, email: e} )}
+            label="email" />
+
+            <TextInput mode="outlined" 
+            label="Senha" secureTextEntry={showPass} 
+            value={campos.senha}
+            onChangeText={(e)=> setCampos( {...campos, senha: e} )}
+            right={<TextInput.Icon icon={showPass?'eye':'eye-closed'} 
+            onPress={ ()=> setShowPass(!showPass)} />}/>
+
+          </Card.Content>
+          <Card.Actions>
+            <Button onPress={handleLogin} >Ok</Button>
+
+          </Card.Actions>
+        </Card>
+
+        </Pressable>
+        </KeyboardAvoidingView>
+
+          <SafeAreaProvider>
+                  <Snackbar
+                        visible={showSnack.show}
+                        >
+                        {showSnack.message}
+                      </Snackbar>
+          </SafeAreaProvider>
+
+    </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    paddingTop: 200,
+    backgroundColor: '#000',
+    width: '100%',
+  }
+})
+
+export default LoginView
