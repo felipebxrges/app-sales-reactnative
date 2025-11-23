33c35756b18dc65ecf46a60fddb4558fb27d9c24
felipebxrges <borgesfelipe2005@gmail.com>
2025-11-22 22:50:13 -0300

    carrinho controller updates

--- sales-app/src/controller/CarrinhoController.js
+++ sales-app/src/controller/CarrinhoController.js
@@ -6,106 +6,115 @@ import Camisa from "../model/camisa";
 import ItemCarrinho from "../model/itemcarrinho";
 
 export const useCarrinhoController = () => {
-    const [carrinho, setCarrinho] = useState([])
-
+    const [carrinho, setCarrinho] = useState([]);
     const { logado } = useAuth();
 
     const fetchCarrinho = async () => {
         try {
-            var data = await CarrinhoRepository.GetItensDoCarrinho(logado.id);
-
-            console.log(logado.id);
-            console.log(carrinho);
+            const data = await CarrinhoRepository.GetItensDoCarrinho(logado.id);
 
             if (data.status !== 200) {
-                throw new Error("Erro na requisição")
+                throw new Error("Erro na requisição");
             }
 
-            var json = await data.json();
+            const json = await data.json();
 
             const camisasCompletas = await Promise.all(
                 json.map(async (item) => {
                     const resCamisa = await CamisaRepository.getCamisaById(item.camisaId);
                     const camisaJson = await resCamisa.json();
 
-                    var camisa = new Camisa(camisaJson.id, camisaJson.nome, parseFloat(camisaJson.preco), camisaJson.imagem, camisaJson.idAdmin);
+                    const camisa = new Camisa(
+                        camisaJson.id,
+                        camisaJson.nome,
+                        parseFloat(camisaJson.preco),
+                        camisaJson.imagem,
+                        camisaJson.idAdmin
+                    );
 
-                    return new ItemCarrinho(item.id, logado.id, camisa, item.quantidade);
+                    return new ItemCarrinho(
+                        item.id,
+                        logado.id,
+                        camisa,
+                        item.quantidade
+                    );
                 })
             );
 
             setCarrinho(camisasCompletas);
-            console.log(camisasCompletas);
-            
-        }
-        catch (error) {
-            console.log(error.message)
+
+        } catch (error) {
+            console.log(error.message);
             setCarrinho([]);
         }
-    }
+    };
+
+    const atualizarQuantidade = async (item, novaQuantidade) => {
+        try {
+            const res = await CarrinhoRepository.AtualizaQuantidadeItemCarrinho(
+                item.id,
+                novaQuantidade
+            );
+
+            if (!res.ok) throw new Error("Erro ao atualizar quantidade");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const adicionarAoCarrinho = async (produto) => {
+        const existente = carrinho.find(i => i.camisa.id === produto.id);
 
-        const existe = carrinho.find((i) => i.camisa.id === produto.id);
-
-        if (existe) {
-
-            existe.incrementar();
+        if (existente) {
+            existente.incrementar();
             setCarrinho([...carrinho]);
+            await atualizarQuantidade(existente, existente.quantidade);
+            return;
+        }
 
-            try {
-                const res = await CarrinhoRepository.AtualizaQuantidadeItemCarrinho(existe.id, existe.quantidade);
-                if (!res.ok) throw new Error("Erro ao atualizar quantidade");
-            } catch (err) {
-                console.log(err);                
-            }
-        } else {
-       
-            try {
-                const res = await CarrinhoRepository.AdicionaItemAoCarrinho(logado.id, produto.id);
-                if (!res.ok) throw new Error("Erro ao adicionar item");
-                const json = await res.json(); 
+        try {
+            const res = await CarrinhoRepository.AdicionaItemAoCarrinho(
+                logado.id,
+                produto.id
+            );
 
-                const novoItem = new ItemCarrinho(json.id, logado.id, produto, 1);
-                setCarrinho((prev) => [...prev, novoItem]);
-            } catch (err) {
-                console.log(err);
-            }
+            if (!res.ok) throw new Error("Erro ao adicionar item");
+
+            const json = await res.json();
+            const novoItem = new ItemCarrinho(json.id, logado.id, produto, 1);
+
+            setCarrinho(prev => [...prev, novoItem]);
+
+        } catch (err) {
+            console.log(err);
         }
     };
 
     const removerDoCarrinho = async (produtoId) => {
-        const existente = carrinho.find((i) => i.id === produtoId);
-        console.log(existente);
+        const existente = carrinho.find(i => i.id === produtoId);
         if (!existente) return;
 
         if (existente.quantidade > 1) {
-            try {
-                existente.decrementar();
-                setCarrinho([...carrinho]);
+            existente.decrementar();
+            setCarrinho([...carrinho]);
+            await atualizarQuantidade(existente, existente.quantidade);
+            return;
+        }
 
-                const res = await CarrinhoRepository.AtualizaQuantidadeItemCarrinho(existente.id, existente.quantidade);
+        try {
+            const res = await CarrinhoRepository.RemoveItemDoCarrinho(existente.id);
 
-                if (!res.ok) throw new Error("Erro ao atualizar quantidade");
+            if (!res.ok) throw new Error("Erro ao remover item");
 
-            } catch (err) {
-                console.log(err);
-            }
-        } else {
-            try {
-                var res = await CarrinhoRepository.RemoveItemDoCarrinho(existente.id);
+            setCarrinho(prev => prev.filter(i => i.id !== existente.id));
 
-                if (!res.ok) throw new Error("Erro ao remover item");
-
-                setCarrinho((prev) => prev.filter((i) => i.id !== existente.id));
-            } catch (err) {
-                console.log(err);
-            }
+        } catch (err) {
+            console.log(err);
         }
     };
 
     return {
         states: { carrinho },
         actions: { setCarrinho, fetchCarrinho, adicionarAoCarrinho, removerDoCarrinho }
-    }
-}
+    };
+};
